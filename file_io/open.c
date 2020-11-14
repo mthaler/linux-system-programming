60d654b7cfdf9e2cbb0374be69f0edd4644d31f8
Michael Thaler <michael.thaler@dlr.de>
2020-11-14 08:01:09 +0100

    Added open.c.

---
+++ file_io/open.c
@@ -0,0 +1,11 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdio.h>
+
+int main() {
+    int fd;
+    fd = open("/tmp/test.txt", O_RDONLY);
+    if (fd == -1)
+        perror ("open");
+}
